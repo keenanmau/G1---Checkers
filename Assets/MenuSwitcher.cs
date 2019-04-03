bb5dbb88b57bb0fcc677f976e2e22c7732c642bd
keenanmau <keenanmau@gmail.com>
2019-04-03 16:40:17 -0700

    Menu Switching
    Created Title card with title and begin button. Also a corner button. Each respective button deactivated and activateds the other menu

---
+++ Assets/MenuSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSwitcher : MonoBehaviour
+{
+    public GameObject MainMenuCanvas;
+    public GameObject CornerButton;
+    private bool trigger = true;
+
+    void Start() {
+        MainMenuCanvas = this.gameObject.transform.GetChild(0).gameObject;
+
+        CornerButton = this.gameObject.transform.GetChild(1).gameObject;
+        string name1 = MainMenuCanvas.transform.name;
+        string name2 = CornerButton.transform.name;
+        Debug.Log("Main Menu Canvas: " + name1 + "\n Button Canvas: " + name2);
+
+        MainMenuCanvas.SetActive(trigger);
+        CornerButton.SetActive(!trigger);
+    }
+
+
+ 
+}
